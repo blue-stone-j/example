aa190f196d7a1186fb6d1346e923d1ef3052d86d
blue-stone <qingshiji96@gmail.com>
2025-03-18 20:07:54 +0800

    rename varibles

--- cpp/src/grammar/delete_element_array.cpp
+++ cpp/src/grammar/delete_element_array.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cstring>
 
-//	实例086  利用指针删除数组中的指定元素（指针移动）
+// 利用指针删除数组中的指定元素（指针移动）
 int main()
 {
-  char ch[] = "ajfdklsafj";
-  char *ptr = ch;                         // 获取ch的首地址
-  ptr += strlen(ch) / 2;                  // 向后移动strlen(ch)/2位
+  char ch[]     = "ajfdklsafj";
+  char *pointer = ch;                     // 获取ch的首地址
+  pointer += strlen(ch) / 2;              // 向后移动strlen(ch)/2位
   for (size_t i = 3; i < strlen(ch); i++) // 删除第4个元素
   {
     ch[i] = ch[i + 1];
