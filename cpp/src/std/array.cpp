b6ace0fc498ae300610b8771c49d4d9203507b2f
blue-stone <qingshiji96@gmail.com>
2025-04-21 00:24:07 +0800

    delete evenly from vector

---
+++ cpp/src/std/array.cpp
@@ -0,0 +1,7 @@
+#include <array>
+#include <vector>
+
+int main()
+{
+  std::array<std::array<std::vector<int>, 20>, 20> grid{};
+}
---
+++ cpp/src/std/vector.cpp
@@ -0,0 +1,36 @@
+// This program demonstrates how to delete elements from a vector evenly until it reaches a target size.
+
+#include <iostream>
+#include <vector>
+
+void deleteEvenly(std::vector<int> &vec, std::size_t targetSize)
+{
+  while (vec.size() > targetSize)
+  {
+    std::size_t removeCount = vec.size() - targetSize;
+    std::size_t step        = vec.size() / removeCount; // Calculate step size
+
+    for (std::size_t i = 0; i < removeCount; ++i)
+    {
+      std::size_t index = i * step;
+      if (index >= vec.size()) break;
+      vec.erase(vec.begin() + index);
+    }
+  }
+}
+
+int main()
+{
+  std::vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  deleteEvenly(vec, 5); // Reduce the vector size to 5 elements
+
+  // Print the remaining elements
+  for (int num : vec)
+  {
+    std::cout << num << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
