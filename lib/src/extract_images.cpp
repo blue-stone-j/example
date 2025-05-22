2a5341f7491ff9ff9e937482a8f799964d0e5f2a
blue-stone <qingshiji96@gmail.com>
2025-05-22 22:12:48 +0800

    Create extract_images.cpp

---
+++ lib/src/extract_images.cpp
@@ -0,0 +1,69 @@
+// Extract images from a ROS bag file
+
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <boost/filesystem.hpp>
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+  if (argc < 3)
+  {
+    std::cerr << "Usage: " << argv[0] << " <bag_file> <image_topic>" << std::endl;
+    return 1;
+  }
+
+  std::string bag_file    = argv[1];
+  std::string image_topic = argv[2];
+  std::string out_folder  = "extracted_images";
+
+  boost::filesystem::create_directory(out_folder);
+
+  rosbag::Bag bag;
+  try
+  {
+    bag.open(bag_file, rosbag::bagmode::Read);
+  }
+  catch (rosbag::BagException &e)
+  {
+    std::cerr << "Error opening bag file: " << e.what() << std::endl;
+    return 1;
+  }
+
+  rosbag::View view(bag, rosbag::TopicQuery(image_topic));
+  size_t count = 0;
+
+  for (const rosbag::MessageInstance &m : view)
+  {
+    auto image_message = m.instantiate<sensor_msgs::Image>();
+    if (!image_message)
+    {
+      continue;
+    }
+
+    try
+    {
+      cv::Mat image = cv_bridge::toCvCopy(image_message, image_message->encoding)->image;
+      std::ostringstream filename;
+      filename << out_folder << "/" << std::fixed << std::setprecision(9)
+               << image_message->header.stamp.toSec() << ".png";
+
+      cv::imwrite(filename.str(), image);
+      ++count;
+    }
+    catch (const cv_bridge::Exception &e)
+    {
+      std::cerr << "cv_bridge exception: " << e.what() << std::endl;
+    }
+  }
+
+  std::cout << "Saved " << count << " images to: " << out_folder << std::endl;
+  bag.close();
+  return 0;
+}
